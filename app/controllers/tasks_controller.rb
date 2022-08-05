6f63e7a34f23308d7aa935929fa060ab7f7d02f3
fqqk <fqqk.work68@gmail.com>
2022-08-05 16:22:24 +0900

    create,index,destroy,private内のメソッドの定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,45 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
   before_action :authenticate_user!
   def index
+    @tasks = Task.all
   end
 
   def new
     @task = Task.new
   end
 
+  def create
+    @task = Task.create(task_params)
+    if @task.save
+      redirect_to task_url(@blog), notice: '新しいタスクを登録しました！'
+    else
+      render :new, alert: "タスクの登録に失敗しました。やり直してください"
+    end
+  end
+
   def show
+
   end
 
   def edit
+  end
+
+  def destroy
+    if @task.destroy
+      redirect_to tasks_url, notice: "ブログを削除しました"
+    else
+      render task_url, alert: "ブログの削除に失敗しました"
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content, :deadline, :status)
   end
 end
