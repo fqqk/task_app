501346b1916ed1560153b55d39641d7f17a29f8a
fqqk <fqqk.work68@gmail.com>
2022-08-05 16:20:20 +0900

    複数タスクに紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable #:confirmableを追記
+  has_many :tasks, dependent: :destroy
 end
