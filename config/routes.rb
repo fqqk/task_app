8847742146c3cdbf3c26557c382315c20fa07373
fqqk <fqqk.work68@gmail.com>
2022-08-09 12:09:13 +0900

    不要なdeviseコントローラー削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   resources :tasks
   resources :mytasks, only:[:index]
-  devise_for :users, :controllers => {
-    :confirmations => 'users/confirmations',
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions',
-    :passwords => 'users/passwords'
-   }
+  devise_for :users
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
