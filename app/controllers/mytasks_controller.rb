59a0836a51a02fa1297fdb89e6f17b666c2335c3
fqqk <fqqk.work68@gmail.com>
2022-08-10 15:06:26 +0900

    未対応、対応中タスクのみ取得

--- app/controllers/mytasks_controller.rb
+++ app/controllers/mytasks_controller.rb
@@ -1,6 +1,6 @@
 class MytasksController < ApplicationController
   before_action :authenticate_user!
   def index
-    @mytasks = current_user.tasks
+    @mytasks = current_user.tasks.not_complete
   end
 end
