88e8ee464a5940e5dc341624a7be7a107ce323a1
fqqk <fqqk.work68@gmail.com>
2022-08-12 16:11:31 +0900

    バリデーションの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :user
   enum status: { incomplete: 0, doing: 1, complete: 2 }
+  paginates_per 6
+  validates :title, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :deadline, presence: true
+  validates :status, presence: true
 end
