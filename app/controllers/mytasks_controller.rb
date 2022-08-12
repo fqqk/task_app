6cf7bbda86afb1099a8127dde2906b73eda662d5
fqqk <fqqk.work68@gmail.com>
2022-08-12 16:12:06 +0900

    ページネーション

--- app/controllers/mytasks_controller.rb
+++ app/controllers/mytasks_controller.rb
@@ -1,6 +1,6 @@
 class MytasksController < ApplicationController
   before_action :authenticate_user!
   def index
-    @mytasks = current_user.tasks.not_complete
+    @mytasks = current_user.tasks.not_complete.page(params[:page]).limit(6)
   end
 end
