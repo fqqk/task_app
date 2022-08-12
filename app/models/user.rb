0ea77b73767a1c6861325123ff8b00e0cd9836ab
fqqk <fqqk.work68@gmail.com>
2022-08-12 16:11:12 +0900

    バリデーションの追加とコメント削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable #:confirmableを追記
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :tasks, dependent: :destroy
+  validates :name, presence: true
 end
