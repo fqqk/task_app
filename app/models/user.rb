4c648d5984d7ced144e6fdf020d32387bc5801f2
fqqk <fqqk.work68@gmail.com>
2022-08-15 13:24:17 +0900

    コメントと一対多

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :tasks, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :name, presence: true
 end
