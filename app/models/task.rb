03a5af8ce99f41b8bc43d34c54887823a812d209
fqqk <fqqk.work68@gmail.com>
2022-08-16 15:39:04 +0900

    enumの削除に伴い、not_completeが使えなくなったのでwhereに書き換え

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,15 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  enum status: { incomplete: 0, doing: 1, complete: 2 }
   paginates_per 6
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :deadline, presence: true
   validates :status, presence: true
 
-  scope :latest, -> {not_complete.order(updated_at: :desc)}
-  scope :old, -> {not_complete.order(updated_at: :asc)}
-  scope :emergency, -> {not_complete.order(deadline: :asc)}
+  scope :latest, -> {where.not(status:"complete").order(updated_at: :desc)}
+  scope :old, -> {where.not(status:"complete").order(updated_at: :asc)}
+  scope :emergency, -> {where.not(status:"complete").order(deadline: :asc)}
 
 
 
