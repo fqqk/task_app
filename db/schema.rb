a037d34cc704c9616c48a1190848ded1df0a1eae
fqqk <fqqk.work68@gmail.com>
2022-08-16 15:37:12 +0900

    statusのデフォルト値を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_091125) do
+ActiveRecord::Schema.define(version: 2022_08_16_062352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 2022_08_15_091125) do
     t.datetime "deadline", null: false
     t.bigint "tasks_id"
     t.bigint "user_id"
-    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "incomplete", null: false
     t.index ["tasks_id"], name: "index_tasks_on_tasks_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
