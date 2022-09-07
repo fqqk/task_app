1de2e67a7c7136ce92fc5db277f85a75c1da5141
fqqk <fqqk.work68@gmail.com>
2022-09-07 13:05:48 +0900

    Revert "モデルのバリデーションテスト"
    This reverts commit 435c291c96ba48fce1ebad13d6a7eefa2f265a23.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  describe '#create' do
-    it "commentが存在すれば登録できること" do
-      expect(create(:comment)).to be_valid
-    end
-
-    it "commentがなければ登録できないこと" do
-      comment = build(:comment, comment: nil)
-      comment.valid?
-      expect(comment.errors[:comment]).to include("を入力してください")
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
