8f4c1a1b3d80caf475f707c26373f1844355018e
fqqk <fqqk.work68@gmail.com>
2022-09-09 18:36:33 +0900

    バリデーションテスト

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    it "title, content, deadline, statusが存在すれば登録できること" do
+      expect(create(:task)).to be_valid
+    end
+
+    it "titleがなければ登録できないこと" do
+      task = build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("を入力してください")
+    end
+
+    it "contentがなければ登録できないこと" do
+      task = build(:task, content: nil)
+      task.valid?
+      expect(task.errors[:content]).to include("を入力してください")
+    end
+
+    it "deadlineがなければ登録できないこと" do
+      task = build(:task, deadline: nil)
+      task.valid?
+      expect(task.errors[:deadline]).to include("を入力してください")
+    end
+
+    it "statusがなければ登録できないこと" do
+      task = build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("を入力してください")
+    end
+
+    # it "スラックに通知すること" do
+
+    # end
+  end
 end
