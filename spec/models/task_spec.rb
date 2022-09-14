1efec417b59d4684e955bd703f0049492e6b70ff
fqqk <fqqk.work68@gmail.com>
2022-09-14 15:08:37 +0900

    テスト微調整

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,37 +9,37 @@ RSpec.describe Task, type: :model do
     it "titleがなければ登録できないこと" do
       task = build(:task, title: nil)
       task.valid?
-      expect(task.errors[:title]).to include("を入力してください")
+      expect(task.errors.full_messages).to include("Titleを入力してください")
     end
 
     it "contentがなければ登録できないこと" do
       task = build(:task, content: nil)
       task.valid?
-      expect(task.errors[:content]).to include("を入力してください")
+      expect(task.errors.full_messages).to include("Contentを入力してください")
     end
 
     it "contentが140文字以上であれば登録できないこと" do
       task = build(:task, content: 'a'*141)
       task.valid?
-      expect(task.errors[:content]).to include("は140文字以内で入力してください")
+      expect(task.errors.full_messages).to include("Contentは140文字以内で入力してください")
     end
 
     it "deadlineがなければ登録できないこと" do
       task = build(:task, deadline: nil)
       task.valid?
-      expect(task.errors[:deadline]).to include("を入力してください")
+      expect(task.errors.full_messages).to include("Deadlineを入力してください")
     end
 
     it "deadlineが現在以降でなければ登録できないこと" do
       task = build(:task, deadline: Time.zone.now.prev_year)
       task.valid?
-      expect(task.errors[:deadline]).to include("は現在以降のものを選択してください")
+      expect(task.errors.full_messages).to include("Deadlineは現在以降のものを選択してください")
     end
 
     it "statusがなければ登録できないこと" do
       task = build(:task, status: nil)
       task.valid?
-      expect(task.errors[:status]).to include("を入力してください")
+      expect(task.errors.full_messages).to include("Statusを入力してください")
     end
 
     # it "スラックに通知すること" do
